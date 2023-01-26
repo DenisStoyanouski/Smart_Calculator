0774b0d528732e13cd126fb0eb18a9661119a203
Denis Stoyanouski <vandenst@tut.by>
2023-01-26 10:15:44 +0300

    add method processInput class Engine

--- src/calculator/Engine.java
+++ src/calculator/Engine.java
@@ -9,14 +9,30 @@ public class Engine {
     public static void processInput() {
         while (true) {
             String[] arguments = input().split("\\s");
+            switch (arguments.length) {
+                case 0 : break;
+                case 1 :
+                    if ("/exit".equals(arguments[0])) {
+                        System.exit(0);
+                    } else {
+                        System.out.println(arguments[0]);
+                    }
+                    break;
+                case 2 :
+                    try {
+                        System.out.println(sum(Integer.parseInt(arguments[0]), Integer.parseInt(arguments[1])));
+                    } catch (NumberFormatException e) {
+                        System.out.println("You need input two digits");
+                    }
+            }
         }
 
     }
-
     private static String input() {
         return scanner.nextLine();
     }
-    public static int sum(int a, int b) {
+
+    private static int sum(int a, int b) {
         return a + b;
     }
 }
