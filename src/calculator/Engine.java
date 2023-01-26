c37b92e9dd19071600a52a2f60dc0f93f2b94f4b
Denis Stoyanouski <vandenst@tut.by>
2023-01-26 21:24:25 +0300

    add method subtract class Engine

--- src/calculator/Engine.java
+++ src/calculator/Engine.java
@@ -9,7 +9,7 @@ public class Engine {
     public static void processInput() {
         while (true) {
             //System.out.print("> ");
-            String[] arguments = input().split("\\s");
+            String[] arguments = input().split("\\s+");
             try {
                 switch (arguments.length) {
                     case 0 : break;
@@ -17,7 +17,7 @@ public class Engine {
                         if ("/exit".equals(arguments[0])) {
                             exit();
                         } else if ("/help".equals(arguments[0])){
-                            System.out.println("The program calculates the sum of numbers");
+                            System.out.println("The program calculates the sum and subtraction of numbers");
                         } else if (arguments[0].isEmpty()) {
                             break;
                         } else {
@@ -49,23 +49,25 @@ public class Engine {
         for (int i = 1; i < expression.length; i++) {
             String currentSymbol = expression[i];
             if (currentSymbol.matches("\\d")) {
-                switch (currentOperator) {
-                    case "+" -> currentResult = addition(currentResult, Integer.parseInt(currentSymbol));
-                    case "-" -> currentResult = subtraction(currentResult, Integer.parseInt(currentSymbol));
-                    default -> System.out.println("Unknown operation");
+                String[] operations = currentOperator.split("");
+                for(String operation : operations) {
+                    switch(operation) {
+                        case "+" -> currentResult = add(currentResult, Integer.parseInt(currentSymbol));
+                        case "-" -> currentResult = subtract(currentResult, Integer.parseInt(currentSymbol));
+                    }
                 }
-            } else if (currentSymbol.matches("[+-/*]")) {
-                currentOperator = currentSymbol;
+            } else {
+                currentOperator = currentSymbol.replaceAll("--", "+");
             }
         }
         return currentResult;
     }
 
-    private static int addition(int a, int b) {
+    private static int add(int a, int b) {
         return a + b;
     }
 
-    private static int subtraction(int a, int b) {
+    private static int subtract(int a, int b) {
         return a - b;
     }
 }
