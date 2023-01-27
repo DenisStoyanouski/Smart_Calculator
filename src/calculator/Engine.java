3897caa74974f5a3e380e5edfcb5ead878e417bf
Denis Stoyanouski <vandenst@tut.by>
2023-01-27 17:19:51 +0300

    refactor method processInput class Engine

--- src/calculator/Engine.java
+++ src/calculator/Engine.java
@@ -1,6 +1,9 @@
 package calculator;
 
+import java.sql.SQLOutput;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -11,36 +14,83 @@ public class Engine {
 
     private static String line;
 
+    private static Pattern number = Pattern.compile("(-|\\+)?\\d*");
+    private static Pattern expression = Pattern.compile("(-|\\+)?\\d+(\\s+?[+-]+\\s+?\\d+)*");
+    private static Pattern command = Pattern.compile("/[a-zA-Z]+");
+
+
+    private static Map<String, Integer> variables = new HashMap<>();
+
     public static void processInput() {
 
         while (true) {
             line = input();
-            if (line.isEmpty() || isExpressionOrCommand(line)) {
-                String[] arguments = line.split("\\s+");
-                try {
-                    switch (arguments.length) {
-                        case 0 : break;
-                        case 1 :
-                            if ("/exit".equals(arguments[0])) {
-                                exit();
-                            } else if ("/help".equals(arguments[0])){
-                                System.out.println("The program calculates the sum and subtraction of numbers");
-                            } else if (arguments[0].isEmpty()) {
-                                break;
-                            } else {
-                                System.out.println(Integer.parseInt(arguments[0]));
-                            }
-                            break;
-                        default: System.out.println(calculate(arguments));
-                            break;
-                    }
-                } catch (NumberFormatException e) {
-                    System.out.println("You need input digits");
+            Matcher matcherCom = command.matcher(line);
+            Matcher matcherEx = expression.matcher(line);
+            Matcher matcherNumber = number.matcher(line);
+
+            if (line.isEmpty()) {
+                continue;
+            } else if (matcherNumber.matches()){
+                int number = Integer.parseInt(line);
+                System.out.println(number);
+            } else if (matcherCom.matches()) {
+                getCommand(line);
+            } else if (line.matches("[a-zA-Z].*")) {
+                getVariable(line);
+            } else {
+                if (matcherEx.matches()) {
+                    System.out.println(calculate(line));
+                } else {
+                    System.out.println("Invalid expression");
                 }
+
             }
         }
-
     }
+
+    private static void getVariable(String line) {
+        String var;
+        int value;
+
+        Pattern identifier = Pattern.compile("[a-zA-Z]*\\s*?");
+        Pattern assignment = Pattern.compile("\\s*?\\d+\\s*?");
+        Pattern expression = Pattern.compile(identifier + "=" + assignment);
+
+        Matcher mExpression = expression.matcher(line);
+        Matcher matcherVar = identifier.matcher(line);
+        Matcher mIdentifier = identifier.matcher(line).region(0, line.indexOf("="));
+        Matcher mAssignment = assignment.matcher(line).region(line.indexOf("=") + 1, line.length());
+
+        if (matcherVar.matches()) {
+            var = line.strip();
+            if (variables.containsKey(var)) {
+                System.out.println(variables.get(var));
+            } else {
+                System.out.println("Unknown variable");
+            }
+        } else if (!mIdentifier.matches()) {
+            System.out.println("Invalid identifier");
+        } else if (!mAssignment.find()){
+            System.out.println("Invalid assignment");
+        } else if (mExpression.matches()) {
+            var = line.replaceAll("=" + assignment, "").strip();
+            value = Integer.parseInt(line.replaceAll(identifier + "=", "").strip());
+            variables.put(var, value);
+            System.out.println(variables.get(var));
+        }
+    }
+
+    private static void getCommand(String line) {
+        if (line.matches("\\s*/exit\\s*")) {
+            exit();
+        } else if (line.matches("\\s*/help\\s*")) {
+            System.out.println("The program calculates the sum and subtraction of numbers");
+        } else {
+            System.out.println("Unknown command");
+        }
+    }
+
     private static String input() {
         return scanner.nextLine();
     }
@@ -50,23 +100,8 @@ public class Engine {
         System.exit(0);
     }
 
-    private static boolean isExpressionOrCommand(String line) {
-        boolean isValid = true;
-        Pattern expression = Pattern.compile(".?\\d+(\\s+?[+-]+\\s+?\\d+)*");
-        Pattern command = Pattern.compile("(/exit)|(/help)");
-        Matcher matcherCom = command.matcher(line);
-        Matcher matcherEx = expression.matcher(line);
-        if (line.matches("/.+") && !matcherCom.matches()) {
-            System.out.println("Unknown command");
-            isValid = false;
-        } else if (!line.matches("/.+") && !matcherEx.matches()) {
-            System.out.println("Invalid expression");
-            isValid = false;
-        }
-        return isValid;
-    }
-
-    private static int calculate(String[] expression) {
+    private static int calculate(String line) {
+        String[] expression = line.split("\\s+");
         //System.out.print(Arrays.toString(expression));
         int currentResult = Integer.parseInt(expression[0]);
         String currentOperator = "+";
@@ -74,8 +109,11 @@ public class Engine {
             String currentSymbol = expression[i];
             if (currentSymbol.matches("\\d+")) {
                 switch(currentOperator) {
-                    case "+" -> currentResult = add(currentResult, Integer.parseInt(currentSymbol));
-                    case "-" -> currentResult = subtract(currentResult, Integer.parseInt(currentSymbol));
+                    case "+" : currentResult = add(currentResult, Integer.parseInt(currentSymbol));
+                    break;
+                    case "-" : currentResult = subtract(currentResult, Integer.parseInt(currentSymbol));
+                    break;
+                    default : break;
                 }
             } else {
                 /*Pattern patternPlus = Pattern.compile("([+]+)|((-{2})+)");
